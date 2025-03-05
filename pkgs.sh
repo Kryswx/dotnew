5cee745aa7431f2565d080c02d65b29a18a63bbf
RaininAcid <145204364+RaininAcid@users.noreply.github.com>
2025-03-05 17:58:20 +0200

    Add files via upload

---
+++ pkgs.sh
@@ -0,0 +1,4 @@
+pacman -S mc ttf-jetbrains-mono curl ttf-font-awesome otf-font-awesome fish pkgfile ttf-dejavu powerline-fonts inetutils pavucontrol nwg-look fastfetch thunar 
+git clone https://github.com/vinceliuice/Graphite-gtk-theme
+git clone https://aur.archlinux.org/yay.git
+echo "Enter:	cd yay && makepkg -si"
